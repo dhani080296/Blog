b5e938374553ac48b9e56ec0f5cec797c108892b
dhani080296 <aramdhani14@gmail.com>
2017-05-10 13:58:06 +0700

    UserController

--- app/Http/Controllers/Backend/UsersController.php
+++ app/Http/Controllers/Backend/UsersController.php
@@ -9,7 +9,11 @@ use App\Http\Requests;
 use App\Post;
 class UsersController extends BackendController
 {
-    
+    protected $uploadPath;
+    public function __construct(){
+        parent::__construct();
+        $this->uploadPath=public_path(config('cms.image.directory'));
+    }
 
     public function index(){
     	$users=User::orderBy('name')->paginate($this->limit);
@@ -22,7 +26,7 @@ class UsersController extends BackendController
     }
     public function store(Requests\UserStoreRequest $request){
     $data = $request->all();
-    $data['password'] = crypt($data['password']);
+    $data['password'] = bcrypt($data['password']);
     User::create($data);
     return redirect("/backend/users")->with("message","New user was created successfully!");
     }
@@ -31,7 +35,11 @@ class UsersController extends BackendController
     return view("backend.users.edit",compact('user'));	
     }
     public function update(Requests\UserUpdateRequest $request,$id){
-    user::findOrFail($id)->update($request->all());
+    $user=user::findOrFail($id);
+     $user->fill([
+            'password' => bcrypt($request['password'])
+        ])->save();
+    
     return redirect("/backend/users")->with("message","User was Updated successfully!");
     }
    public function destroy(Requests\UserDestroyRequest $request,$id){
@@ -41,12 +49,13 @@ class UsersController extends BackendController
     $selectedUser=$request->selected_user;
     if($deleteOption=="delete"){
         // delete posts
-        $user->posts()->delete();
+        $user->posts()->withTrashed()->forceDelete();
+        $this->removeImage($user->posts()->withTrashed()->image);
         // delete user
     }else if($deleteOption=="attribute"){
       $user->posts()->update(['author_id'=>$selectedUser]);  
     }
-    // $User->delete();
+    $user->delete();
     return redirect("/backend/users")->with("message","User was Deleted Successfully!");
     }
     public function confirm(Requests\UserDestroyRequest $request,$id){
@@ -54,5 +63,15 @@ class UsersController extends BackendController
     $user =User::findOrFail($id);
     $users=User::where('id','!=',$user->id)->pluck('name','id');
     return view("backend.users.confirm",compact('user','users'));
+    }
+    private function removeImage($image){
+        if(!empty($image)){
+            $imagepath=$this->uploadPath.'/'.$image;
+            $extension=substr(strrchr($image,'.'),1);
+            $thumbnail=str_replace(".{$extension}", "_thumb.{$extension}",$image);
+            $thumbnailpath=$this->uploadPath.'/'.$thumbnail;
+            if(file_exists($imagepath)) unlink($imagepath);
+            if(file_exists($thumbnailpath)) unlink($thumbnailpath);
+        }
     }
 }
